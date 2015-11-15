adc238b40fc60530d633ba8c94d8ab74fdaf3e87
iwagaki <iwagaki@users.sourceforge.net>
2015-11-16 00:21:58 +0900

    Added gtest cases

--- test/test.cpp
+++ test/test.cpp
@@ -1,13 +1,34 @@
 #include <gtest/gtest.h>
 #include "../FilePointer.h"
 
-TEST(FileReadPointer, FileAccess) { 
+TEST(FileReadPointer, CannotOpenInvalidFileName) {
+    FileReadPointer reader("data/hogehoge");
+    ASSERT_EQ(NULL, static_cast<FILE*>(reader));
+}
+
+TEST(FileReadPointer, CannotOpenInvalidFileName_fp) {
+    FileReadPointer reader("data/hogehoge");
+    ASSERT_EQ(NULL, reader.fp());
+}
+
+TEST(FileReadPointer, FileAccess) {
     {
         FileReadPointer reader("../LICENSE");
         ASSERT_EQ(0, fseeko(reader, 0, SEEK_SET));
     }
 }
- 
+
+TEST(FileWritePointer, CannotOpenReadOnlyFile) {
+    FileWritePointer writer("data/ro_data");
+    ASSERT_EQ(NULL, static_cast<FILE*>(writer));
+}
+
+TEST(FileWritePointer, CannotOpenReadOnlyFile_fp) {
+    FileWritePointer writer("data/ro_data");
+    ASSERT_EQ(NULL, writer.fp());
+}
+
+
 // int main(int argc, char **argv) {
 //     testing::InitGoogleTest(&argc, argv);
 //     return RUN_ALL_TESTS();
